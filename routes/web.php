b5624736281eab6b56807cbcade2bb79d6b2a0c1
enakutsa <enakutsakokouespoir@gmail.com>
2026-06-21 11:06:18 +0000

    ajout route de diagnostic temporaire

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
+
+Route::get('/check-tables', function () {
+    $tables = \Illuminate\Support\Facades\DB::select("SELECT tablename FROM pg_tables WHERE schemaname='public'");
+    return response()->json($tables);
+});
