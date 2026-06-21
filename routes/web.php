286a215fb128a508fd3373845a28ebdde9243363
enakutsa <enakutsakokouespoir@gmail.com>
2026-06-21 11:14:34 +0000

    fix ordre des routes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '.*');
-
 Route::get('/check-tables', function () {
     $tables = \Illuminate\Support\Facades\DB::select("SELECT tablename FROM pg_tables WHERE schemaname='public'");
     return response()->json($tables);
 });
+
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
