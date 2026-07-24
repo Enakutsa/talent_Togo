1393c4b9e69728abab007e72d4fa4a5874f44305
enakutsa <enakutsakokouespoir@gmail.com>
2026-07-24 20:12:19 +0000

    fix: envoi OTP synchrone au lieu de queue (pas de worker en prod)

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -3,11 +3,10 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class OtpMail extends Mailable implements ShouldQueue
+class OtpMail extends Mailable
 {
     use Queueable, SerializesModels;
 
